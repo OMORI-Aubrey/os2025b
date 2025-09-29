b19658935bac55258a0fb041792fdfab9d6ccb8b
JaeYun <sim_518@naver.com>
2025-09-29 14:14:40 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -7,8 +7,10 @@ import (
 )
 
 func main() {
-	// Floor: 내림 Ceil: 올림 Round: 반올림
+	// Floor: 내림, Ceil: 올림, Round: 반올림
 	fmt.Println(math.Round(2.91))
 	// 단어 첫 글자가 대문자가 됨(곧 사라질 함수)
 	fmt.Println(strings.Title("go developer~"))
+	fmt.Println("Kim\nInha\t\"\\") // C언어랑 비슷함
+	fmt.Println('A','가')
 }
