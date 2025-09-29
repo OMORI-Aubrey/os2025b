cb3d1f5d05bd5e32e7af5aee0d66d4d59b76cf2c
JaeYun <sim_518@naver.com>
2025-09-29 15:00:35 +0900

    v0.5 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -21,26 +21,41 @@ func main() {
 		fmt.Println(reflect.TypeOf(true)) //bool
 		fmt.Println(reflect.TypeOf(91)) //int
 	*/
-	// var name string
-	// var id int
-	// name = "Kim Inha"
-	// id = 1000
-	// var name string = "Kim Inha"
-	// var id int = 1000
+	/*
+		// var name string
+		// var id int
+		// name = "Kim Inha"
+		// id = 1000
+		// var name string = "Kim Inha"
+		// var id int = 1000
 
-	// 타입 자동 추론으로 간편하게 선언
-	name := "Kim Inha"
-	id := 1000
+		// 타입 자동 추론으로 간편하게 선언
+		name := "Kim Inha"
+		id := 1000
 
-	// 선언은 두 번 이상 할 수 없음
-	// id := 1000 () // 이미 위에 선언
-	id = 1000
+		// 선언은 두 번 이상 할 수 없음
+		// id := 1000 () // 이미 위에 선언
+		id = 1000
 
-	// gpa := 3.99 // 자동으로 float64 (double)
-	var gpa float32 = 3.99 // 내가 명시한 자료형으로 선언
+		// gpa := 3.99 // 자동으로 float64 (double)
+		var gpa float32 = 3.99 // 내가 명시한 자료형으로 선언
 
-	fmt.Println(name, reflect.TypeOf(name))
-	fmt.Println(id, reflect.TypeOf(id))
-	fmt.Println(gpa, reflect.TypeOf(gpa))
+		fmt.Println(name, reflect.TypeOf(name))
+		fmt.Println(id, reflect.TypeOf(id))
+		fmt.Println(gpa, reflect.TypeOf(gpa))
+	*/
 
+	// Zero values
+	var f64 float64
+	var i16 int16
+	var t bool
+	var s string
+	var i int
+
+	fmt.Println(f64, reflect.TypeOf(f64))
+	fmt.Println(i16, reflect.TypeOf(i16))
+	fmt.Println(t, reflect.TypeOf(t))
+	fmt.Println(s, reflect.TypeOf(s))
+	fmt.Println(i, reflect.TypeOf(i))
+	
 }
