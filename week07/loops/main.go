7c9dcc4b609485fc133308bafe7b9494fb249e4c
JaeYun <sim_518@naver.com>
2025-10-13 15:44:02 +0900

    loops/main.go v0.7 guess

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -84,7 +84,7 @@ func main() {
 	fmt.Println()
 }
 */
-
+/*
 import (
 	"bufio"
 	"fmt"
@@ -116,4 +116,67 @@ func main() {
 		status = "Fail"
 	}
 	fmt.Println(score, status)
+}
+*/
+/*
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	// rand.Seed(55) // 옛날) seed값이 같으면 같은 값이 나옴, 지금은 사용 X
+	dice := rand.Intn(6) + 1 // 1 ~ 6, + 1이 없다면 범위는 0 ~ 5
+	fmt.Println(dice)
+}
+*/
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	// guess challenges players to guess a random number.
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+
+	reader := bufio.NewReader(os.Stdin)
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
+	}
+
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was:", target)
+	}
 }
