09337b33aabe64f4c7afa88771653ec3fb3110c1
JaeYun <sim_518@naver.com>
2025-11-03 15:30:33 +0900

    week10 array v0.2

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
 	"fmt"
 )
@@ -12,9 +13,23 @@ func main() {
 	fmt.Println(arrayBool[1]) // zero value (false)
 
 	arrayInt[1]++
-	arrayInt[1] += 1
+	arrayInt[1] += 1 // zero value + 1
 
 	fmt.Println(arrayInt[1])
+}
+*/
 
+import (
+	"fmt"
+)
 
+func main() {
+	// 크기가 3인 배열 선언
+	var arrayBool [3]bool = [3]bool{true, false, true} // 배열 리터럴
+	var arrayInt [3]int
+
+	fmt.Println(arrayBool[1])
+
+	arrayInt[1] = 2
+	fmt.Println(arrayInt[1])
 }
