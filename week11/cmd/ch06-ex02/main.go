22228a1f96360dec8496365a84796e130d8cae9d
JaeYun <sim_518@naver.com>
2025-11-10 15:38:55 +0900

    week11 slice v0.6

--- week11/cmd/ch06-ex02/main.go
+++ week11/cmd/ch06-ex02/main.go
@@ -8,31 +8,32 @@ import (
 	"strconv"
 )
 
-func GetFloats(fileName string) ([3]float64, error) {
-	var numbers [3]float64
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
 
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
-	i := 0
 	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
-		i++
+
+		numbers = append(numbers, number)
 	}
 
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 	
 	return numbers, nil
@@ -44,7 +45,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	
 	hap := 0.0
 	for i := 0; i < len(weights); i++ {
 		hap = hap + weights[i]
