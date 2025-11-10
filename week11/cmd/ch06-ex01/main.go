abf2f0f8f65662af5393420f61073b0b2ccc5dc1
JaeYun <sim_518@naver.com>
2025-11-10 14:54:45 +0900

    week11 slice v0.4

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -18,12 +18,32 @@ func main() {
 	}
 }
 */
-
+/*
 import "fmt"
 
 func main() {
 	subjects := []string{"Go", "Javascript", "Python", "Linux"}
 	subjectsSlice := subjects[1:3] // slicing
+
+	for _, subject := range subjects {
+		fmt.Println(subject)
+	}
+
+	fmt.Println("=============")
+
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
+	}
+}
+*/
+
+import "fmt"
+
+func main() {
+	subjects := []string{"Go", "Javascript", "Python", "Linux"}
+	subjectsSlice := subjects[:3]
+	// subjectsSlice[0] = "Java" // 원본 값 변경
+	subjects[0] = "Java" // 메모리 주소 공유 (얕은 복사)
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
