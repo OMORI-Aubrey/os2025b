ad3b73b339280ff0542bab3ce7adef0b809b8423
JaeYun <sim_518@naver.com>
2025-11-10 15:01:14 +0900

    week11 slice v0.5

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -22,8 +22,29 @@ func main() {
 import "fmt"
 
 func main() {
-	subjects := []string{"Go", "Javascript", "Python", "Linux"}
+	subjects := [4]string{"Go", "Javascript", "Python", "Linux"}
 	subjectsSlice := subjects[1:3] // slicing
+
+	for _, subject := range subjects {
+		fmt.Println(subject)
+	}
+
+	fmt.Println("=============")
+
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
+	}
+}
+*/
+/*
+import "fmt"
+
+func main() {
+	subjects := [4]string{"Go", "Javascript", "Python", "Linux"}
+	subjectsSlice := subjects[:3]
+	
+	// subjectsSlice[0] = "Java" // 원본 값 변경
+	subjects[0] = "Java" // 메모리 주소 공유 (얕은 복사)
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
@@ -40,10 +61,11 @@ func main() {
 import "fmt"
 
 func main() {
-	subjects := []string{"Go", "Javascript", "Python", "Linux"}
+	subjects := [4]string{"Go", "Javascript", "Python", "Linux"}
 	subjectsSlice := subjects[:3]
-	// subjectsSlice[0] = "Java" // 원본 값 변경
-	subjects[0] = "Java" // 메모리 주소 공유 (얕은 복사)
+
+	subjects[0] = "Java"
+	subjectsSlice = append(subjectsSlice, "Go") // 배열의 길이가 4기 때문에 Linux가 덮어씌어짐
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
