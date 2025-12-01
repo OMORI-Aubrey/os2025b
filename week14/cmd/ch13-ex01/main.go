a3979130a00a411eea4e6c884413ff3723b25042
JaeYun <sim_518@naver.com>
2025-12-01 15:33:17 +0900

    week14 goroutine v0.3

--- week14/cmd/ch13-ex01/main.go
+++ week14/cmd/ch13-ex01/main.go
@@ -20,7 +20,7 @@ func main() {
 	say("메인")     // 메인 고루틴에서 실행
 }
 */
-
+/*
 import (
 	"fmt"
 	"time"
@@ -41,5 +41,37 @@ func main() {
 	go say("고루틴") // 비동기 처리
 	say("메인")
 
+	fmt.Println("전채 실행 시간:", time.Since(start))
+}
+*/
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(msg string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(msg, ":", i)
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+
+func hi(msg string) {
+	time.Sleep(8000 * time.Millisecond)
+	fmt.Println(msg)
+}
+
+
+func main() {
+	start := time.Now()
+
+	// go 키워드를 둘다 줘버리면 아무것도 안나옴
+	// 프로그램 끝날때까지 기다려주지 않기 때문
+	go say("고루틴1") // 6초 걸림 (이 일이 먼저 끝남)
+	go hi("고루틴2") // 8초 걸림
+
+	time.Sleep(10 * time.Second) // 메인 고루틴 대기 시간 줘서 해결
 	fmt.Println("전채 실행 시간:", time.Since(start))
 }
