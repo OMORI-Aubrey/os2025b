df7b9efeae70fef761a3cf68cb6a5bcfc95e9eb0
JaeYun <sim_518@naver.com>
2025-12-01 14:28:20 +0900

    week14 panic v0.2

--- week14/cmd/ch12-ex02/main.go
+++ week14/cmd/ch12-ex02/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	defer fmt.Println("프로그램 종료 - 종료작업")
 	fmt.Println("1. 프로그램 시작")
 
 	panic("심각한 에러 발생!")
+	// 중간에 panic이 발생해도 반드시 defer는 실행
 
 	fmt.Println("2. 이 줄은 실행되지 않음")
 }
