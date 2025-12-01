54c6fc03504ec0e0a9cfc33c69663fd95f9a9b56
JaeYun <sim_518@naver.com>
2025-12-01 14:23:59 +0900

    week14 defer v0.2

--- week14/cmd/ch12-ex01/main.go
+++ week14/cmd/ch12-ex01/main.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
 	"fmt"
 	"os"
@@ -7,14 +8,27 @@ import (
 
 func main() {
 	file, err := os.Open("test.txt")
-	if err != nil {
+	if err != nil { // 파일이 없으면 오픈 실패
 		fmt.Println("파일 오픈 실패:", err)
 		return
 	}
 
 	// defer로 파일 닫기를 미리 예약
+	// defer를 쓰면 지연했다가 마지막에 실행
 	defer file.Close()
 
 	// 파일 작업 수행
 	fmt.Println("파일 읽는 중...")
 }
+*/
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("1st defer") // 4
+	defer fmt.Println("2nd defer") // 3
+	defer fmt.Println("3rd defer") // 2
+	fmt.Println("main logic")      // 1
+}
+// defer가 여러개면 LIFO 형태
+// 선언한 것의 역순으로 동작
