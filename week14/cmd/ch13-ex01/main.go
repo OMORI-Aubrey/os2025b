761b25693bff27ec4452135fb4c5dd5158f52388
JaeYun <sim_518@naver.com>
2025-12-01 15:15:52 +0900

    week14 goroutine v0.2

--- week14/cmd/ch13-ex01/main.go
+++ week14/cmd/ch13-ex01/main.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
 	"fmt"
 	"time"
@@ -12,7 +13,33 @@ func say(msg string) {
 	}
 }
 
+
 func main() {
+	// say("고루틴 키워드 뺌") // 하나 먼저 다 하고 다음꺼 실행
 	go say("고루틴") // 새 고루틴에서 실행
 	say("메인")     // 메인 고루틴에서 실행
+}
+*/
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(msg string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(msg, ":", i)
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+
+func main() {
+	start := time.Now()
+
+	// say("고루틴 뺌")
+	go say("고루틴") // 비동기 처리
+	say("메인")
+
+	fmt.Println("전채 실행 시간:", time.Since(start))
 }
